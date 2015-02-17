513b53698b77c3f548687d040c425a4436fd9c62
Robin Philip <robin@jeevomics.com>
2015-02-17 12:55:46 +0530

    added code fragment in commnets of tutorial for svd using numpy'

--- tutorial_pdz_v5.py
+++ tutorial_pdz_v5.py
@@ -1,3 +1,8 @@
 '''
 Analysis of an alignment of the PDZ domain family
+Calculate SVD of a matrix using numpy functions
+e.g.
+A = floor(random.rand(4,4)*20-10)
+linalg.svd(A)
+for sparse matrices, there are methods in SciPy
 '''
