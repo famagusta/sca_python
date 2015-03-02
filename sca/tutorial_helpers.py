859c79122e7f6b273fec9321094f6a2a7c498cac
Deekshith Juvvadi <deekshith@jeevomics.com>
2015-03-02 16:28:05 +0530

    Improvements regarding readability

--- sca/tutorial_helpers.py
+++ sca/tutorial_helpers.py
@@ -1,7 +1,17 @@
+"""
+Some helper functions that are used in the tutorial files
+"""
 import numpy as np
 
 
 def truncate(alignments, frac_alpha_cutoff):
+    """
+    truncate alignments to sequence positions with percentage gap frequency no greater than
+    100 * (1 - frac_alpha_cutoff)
+    :param alignments:
+    :param frac_alpha_cutoff:
+    :return: alignments array after truncation
+    """
     n_alignments = len(alignments)
     is_letter = np.core.defchararray.isalpha(alignments)
     frac_letters = is_letter.sum(0) * 1.0 / n_alignments
