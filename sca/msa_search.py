f18d3278b8acca9a06a7980cb112c2cd48fee13c
Robin Philip <robin@jeevomics.com>
2015-03-16 23:50:01 +0530

    code clean up

--- sca/msa_search.py
+++ sca/msa_search.py
@@ -7,8 +7,8 @@ import swalign
 import numpy as np
 
 AA = [['ALA', 'ARG', 'ASN', 'ASP', 'CYS', 'GLN', 'GLU', 'GLY', 'HIS', 'ILE',
-       'LEU', 'LYS', 'MET', 'PHE', 'PRO', 'SER', 'THR', 'TRP', 'TYR', 'VAL', 'ASX',
-       'GLX', 'XAA', 'END', 'GAP'],
+       'LEU', 'LYS', 'MET', 'PHE', 'PRO', 'SER', 'THR', 'TRP', 'TYR', 'VAL',
+       'ASX', 'GLX', 'XAA', 'END', 'GAP'],
       ['A', 'R', 'N', 'D', 'C', 'Q', 'E', 'G', 'H', 'I', 'L', 'K', 'M', 'F',
        'P', 'S', 'T', 'W', 'Y', 'V', 'B', 'Z', 'X', '*', '-']]
 MATCH = 2
@@ -23,30 +23,39 @@ def msa_search(residue_list, alignments, truncate_or_not=False):
     the pdb file and chain ID) and every sequence in an MSA (alignment),
     finds the tophit sequence, and then attempts to make a residue number
     list (ats) that relates alignment numbering to structure numbering.  The
-    function works with pdb files that comprise the standard pdb format according to the pdb.org.
+    function works with pdb files that comprise the standard pdb format
+    according to the pdb.org.
     :param residue_list: Residue list obtained by reading a standard pdb file
     using read_pdb() function in sca_read.py file
-    :param alignments: array consisting of alignments read from a .free or .fasta file
+    :param alignments: array consisting of alignments read from a .free or
+    .fasta file
     :param truncate_or_not:
     :return:
     """
-    # TODO: POSSIBLE BUG in MSAsearch.m (Octave version) in the first try-catch block in line 72-73
+    # TODO: POSSIBLE BUG in MSAsearch.m (Octave version) in the first try-catch
+    # block in line 72-73 indsort contains indices corresponding to atomnum_cat
     # indsort contains indices corresponding to atomnum_cat
     # but is used to select entries from atom_fields
     # atom_fields might need to be replaced by atomnum_fields in line 73
     # IF It's not a bug, make sure to change the following code.
 
-    # icode is assumed (in sync with the get_structure method of PDBParser obj.)
+    # icode is assumed (in sync with the get_structure method of PDBParser obj)
     # to be present as third entry in the tuple residue.id
-    resseq_icode_list = [(str(residue.id[1]) + residue.id[2]) for residue in residue_list]
+
+    resseq_icode_list = \
+        [(str(residue.id[1]) + residue.id[2]) for residue in residue_list]
     resname_list = [residue.resname for residue in residue_list]
 
     resseq_icode_unique, indices_unique = unique(resseq_icode_list)
     resname_unique = [resname_list[index] for index in indices_unique]
-    # resseq_icode_unique ~ resnum_list and resname_unique ~ aa_model in the Octave version
 
-    # Replacing the 3-letter resnames in resname_unique by the corresponding 1-letter resnames
-    resname_unique = [AA[1][AA[0].index(resname)] for resname in resname_unique]
+    # resseq_icode_unique ~ resnum_list and resname_unique ~ aa_model in the
+    # Octave version
+
+    # Replacing the 3-letter resnames in resname_unique by the
+    # corresponding 1-letter resnames
+    resname_unique =\
+        [AA[1][AA[0].index(resname)] for resname in resname_unique]
     query = "".join(resname_list)
     scores = []
     for index in range(len(alignments)):
@@ -57,10 +66,12 @@ def msa_search(residue_list, alignments, truncate_or_not=False):
     if truncate_or_not:
         is_letter = np.core.defchararray.isalpha(alignments[max_score_index])
         alignment_trunc = alignments[:, is_letter]
-        print '     truncated alignment using sequence #' + str(max_score_index) + \
+        print '     truncated alignment using sequence #' + \
+            str(max_score_index) + \
             '  (score: ' + str(max_score) + ')'
     else:
-        print 'tophit is sequence #' + str(max_score_index) + '  (score: ' + str(max_score) + ')'
+        print 'tophit is sequence #' + str(max_score_index) + \
+            '  (score: ' + str(max_score) + ')'
 
     if 'alignment_trunc' in locals():
         return max_score_index, alignment_trunc
@@ -70,7 +81,8 @@ def msa_search(residue_list, alignments, truncate_or_not=False):
 
 def smith_water_align(seq1, seq2):
     """
-    Applies Smith-Waterman sequence alignment algo. on the two input sequences and returns the score
+    Applies Smith-Waterman sequence alignment algo. on the two input
+    sequences and returns the score
     :param seq1: aminoacid sequence 1
     :param seq2: aminoacid sequence 2
     :param match: points for each match
@@ -80,7 +92,8 @@ def smith_water_align(seq1, seq2):
     :return:
     """
     scoring = swalign.NucleotideScoringMatrix(MATCH, MISMATCH)
-    align_obj = swalign.LocalAlignment(scoring, GAP_PENALTY, GAP_EXTEND_PENALTY)
+    align_obj = swalign.LocalAlignment(scoring, GAP_PENALTY,
+                                       GAP_EXTEND_PENALTY)
     align = align_obj.align(seq1, seq2)
     return align.score
 
@@ -90,8 +103,8 @@ def unique(my_list):
     Returns a list made out of unique elements in my_list in the same
     order as they appear the first time in my_list
     :param my_list: input list with possible duplicate elements
-    :return: a list with unique elements in the order they appear in the input list,
-     a list with the indices of the unique elements in the input list
+    :return: a list with unique elements in the order they appear in the input
+    list, a list with the indices of the unique elements in the input list
     """
 
     unique_elems = []
--- sca/sca_read.py
+++ sca/sca_read.py
@@ -6,13 +6,6 @@ from Bio import Alphabet
 
 
 def read_free(filename):
-<<<<<<< HEAD
-    # TODO: there might be a library function in Biopython
-    seqs = []
-    for line in open(filename):
-        (label, seq_str) = line.strip().split()
-        seqs.append(list(seq_str))  # seq-str.ljust(SEQ_LENGTH)
-=======
     """
     Reading .free files
     Input: filename - name of the file
@@ -22,15 +15,11 @@ def read_free(filename):
     seqs = []
     for line in open(filename):
         (_, seq_str) = line.strip().split()
-        seqs.append(list(seq_str)) # seq-str.ljust(SEQ_LENGTH)
->>>>>>> 859c79122e7f6b273fec9321094f6a2a7c498cac
+        seqs.append(list(seq_str))  # seq-str.ljust(SEQ_LENGTH)
     return np.array(seqs, np.character)
 
 
 def read_pdb(filename, chainid):
-<<<<<<< HEAD
-    # TODO: think of a better data structure for the output
-=======
     """
     Reading .pdb file
     Input:
@@ -39,7 +28,6 @@ def read_pdb(filename, chainid):
     Output: ndarray
     TODO: think of a better data structure for the output
     """
->>>>>>> 859c79122e7f6b273fec9321094f6a2a7c498cac
     structure = PDBParser().get_structure('PDB_STRUCT', filename)
     model = structure.child_list[0]
     chain = model.child_dict[chainid]
@@ -48,15 +36,11 @@ def read_pdb(filename, chainid):
 
 
 def read_fasta(filename):
-<<<<<<< HEAD
-    msa = AlignIO.read(filename, 'fasta',
-                       alphabet=Alphabet.Gapped(Alphabet.IUPAC.protein))
-=======
     """
     Reading .fasta files
     Input: filename - name of the file
     Output: ndarray
     """
-    msa = AlignIO.read(filename, 'fasta', alphabet = Alphabet.Gapped(Alphabet.IUPAC.protein))
->>>>>>> 859c79122e7f6b273fec9321094f6a2a7c498cac
+    msa = AlignIO.read(filename, 'fasta',
+                       alphabet=Alphabet.Gapped(Alphabet.IUPAC.protein))
     return np.array([list(rec) for rec in msa], np.character)
